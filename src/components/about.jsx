39c4e0cf6f2545dc8ebd234f197196f4764076a4
Oussama Mzoughi <oussama.mzoughi@dfki.de>
2020-04-11 19:34:36 +0200

    first master version

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,8 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>Lorem ipsum dolor sit amet, odio dapibus cras tortor amet, in non odio mi. Litora odio suspendisse ligula vel, ut vel aliquam vulputate augue magnis in, ut purus nulla convallis ullamcorper proin, cursus mauris risus. Mauris proin, erat mauris elit, placerat blandit vestibulum sem. Elementum consequat eget justo mollis, ut tincidunt, libero lobortis ornare luctus, pharetra varius donec porta quam elit donec. Mauris exercitationem tristique cras.</p>
+                    <p>Lorem ipsum dolor sit amet, odio dapibus cras tortor amet, in non odio mi. Litora odio suspendisse ligula vel, ut vel aliquam vulputate augue magnis in, ut purus nulla convallis ullamcorper proin, cursus mauris risus. Mauris proin, erat mauris elit, placerat blandit vestibulum sem. Elementum consequat eget justo mollis, ut tincidunt, libero lobortis ornare luctus, pharetra varius donec porta quam elit donec. Mauris exercitationem tristique cras. </p>
                     </div>
                 </div>
                 </div>
@@ -38,7 +38,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <p>Lorem ipsum dolor sit amet, odio dapibus cras tortor amet, in non odio mi.</p>
                 </div>
                 </div>
             </div>
@@ -49,7 +49,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                    <p>Lorem ipsum dolor sit amet, odio dapibus cras tortor amet, in non odio mi.</p>
                 </div>
                 </div>
             </div>
@@ -60,7 +60,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <p>Lorem ipsum dolor sit amet, odio dapibus cras tortor amet, in non odio mi.</p>
                 </div>
                 </div>
             </div>
